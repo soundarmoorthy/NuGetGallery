755519ff75d7a481ee65aaecc7a72ce851a22d46
Soundararajan Dhakshinamoorthy <Soundararajan.Dhakshinamoorthy@atmel.com>
2016-06-06 14:50:58 +0530

    remove unused construcutor

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
-@msbuild build.msbuild /tv:14.0 /p:VisualStudioVersion=14.0 /p:ToolsVersion=14.0 %*
+@msbuild build.msbuild /tv:14.0 /verbosity:q /p:VisualStudioVersion=14.0 /p:ToolsVersion=14.0 %*
+
