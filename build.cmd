c6f49eeca5f2cdb849f900ecb9d37afe74091034
Soundararajan Dhakshinamoorthy <Soundararajan.Dhakshinamoorthy@atmel.com>
2016-06-07 10:34:02 +0530

    Set verbosity level to defaults

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
-@msbuild build.msbuild /tv:14.0 /verbosity:q /p:VisualStudioVersion=14.0 /p:ToolsVersion=14.0 %*
+@msbuild build.msbuild /tv:14.0  /p:VisualStudioVersion=14.0 /p:ToolsVersion=14.0 %*
