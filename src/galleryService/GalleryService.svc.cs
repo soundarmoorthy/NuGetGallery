ac848ee3c9cc36c5681674e8c73d8d1ff5a72e8b
Usha <usha.balasubramanian@atmel.com>
2016-06-08 16:37:05 +0530

    Fixed sorting extension list based author,title etc

--- src/galleryService/GalleryService.svc.cs
+++ src/galleryService/GalleryService.svc.cs
@@ -51,7 +51,7 @@ namespace Studio.Gallery
         {
             return new[]
                    {
-                       new IdeCategory { Title = "Toolchains" }, new IdeCategory {Title="Utilities"},new IdeCategory {Title = "Software Libraries"}
+                       new IdeCategory { Title = "ToolChain" }, new IdeCategory {Title="Debugging"},new IdeCategory {Title = "Wireless"}
                    };
         }
 
@@ -96,6 +96,31 @@ namespace Studio.Gallery
             return null;
         }
 
+        private OrderByEnum getOrderBy(string orderByClause)
+        {
+            var orderBy = OrderByEnum.Ranking;
+
+            if (orderByClause.Contains("Rating"))
+                orderBy = OrderByEnum.Rating;
+            if (orderByClause.Contains("LastModified"))
+                orderBy = OrderByEnum.LastModified;
+            if (orderByClause.Contains("DownloadCount"))
+                orderBy = OrderByEnum.DownloadCount;
+            if (orderByClause.Contains("Name") || orderByClause.Contains("Title"))
+                orderBy = OrderByEnum.Name;
+            if (orderByClause.Contains("Author"))
+                orderBy = OrderByEnum.Author;
+            return orderBy;
+        }
+
+        private OrderByDirection getOrderByDirection(string orderByClause)
+        {
+            var orderByDirection = OrderByDirection.Desc;
+            if (orderByClause.Contains(" asc"))
+                orderByDirection = OrderByDirection.Asc;
+            return orderByDirection;
+        }
+
         public ReleaseQueryResult SearchReleases2(
             string searchText,
             string whereClause,
@@ -107,18 +132,18 @@ namespace Studio.Gallery
             IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("http://localhost:8080/api/v2/");
             var packages = repo.GetPackages();
             var releases = new List<Studio.Gallery.Model.Release>();
-
             foreach (var pack in packages)
             {
                 DataServicePackage package = (NuGet.DataServicePackage)pack;
                 var release = new Studio.Gallery.Model.Release();
                 release.Project = new Studio.Gallery.Model.Project();
                 release.Reviews = new List<Studio.Gallery.Model.Review>();
+                release.Ratings = new List<Studio.Gallery.Model.ReleaseRating>();
 
                 release.Project.Id = 0;
                 release.Project.Description = package.Description;
                 release.Project.Title = package.Title;
-
+                release.DownloadCount = package.DownloadCount;
                 release.Extension = new Studio.Gallery.Model.Extension();
                 release.Extension.VsixId = package.Id;
                 release.Extension.Author = package.Authors;
@@ -128,18 +153,63 @@ namespace Studio.Gallery
                 release.Extension.VsixVersion = package.Version.ToString();
                 release.Extension.Name = package.Title;
                 release.Extension.MoreInfo = package.Description;
-                
+
                 releases.Add(release);
             }
+            IEnumerable<Model.Release> orderedReleases = null;
 
             var result = new ReleaseQueryResult();
             var host = OperationContext.Current.IncomingMessageHeaders.To.AbsoluteUri;
-            //var root = host.Replace("GalleryService.svc", "");
             var root = "http://localhost:8080";
 
-            result.Releases = releases.Select(r => new Galleries.Domain.Model.Release(r, root)).ToArray();
+            OrderByEnum orderby = getOrderBy(orderByClause);
+            if (orderby == OrderByEnum.DownloadCount)
+            {
+                if (getOrderByDirection(orderByClause) == OrderByDirection.Desc)
+                {
+                    orderedReleases = releases.OrderByDescending(r => r.DownloadCount);
+                }
+                else
+                {
+                    orderedReleases = releases.OrderBy(r => r.DownloadCount);
+                }
+            }
+            else if (orderby == OrderByEnum.Rating || orderby == OrderByEnum.Ranking)
+            {
+                if (getOrderByDirection(orderByClause) == OrderByDirection.Desc)
+                {
+                    orderedReleases = releases.OrderByDescending(r => r.GetAverageRating());
+                }
+                else
+                {
+                    orderedReleases = releases.OrderBy(r => r.GetAverageRating());
+                }
+            }
+            else if (orderby == OrderByEnum.Name)
+            {
+                if (getOrderByDirection(orderByClause) == OrderByDirection.Desc)
+                    orderedReleases = releases.OrderByDescending(r => r.Extension.Name);
+                else
+                {
+                    orderedReleases = releases.OrderBy(r => r.Extension.Name);
+                }
+            }
+            else if (orderby == OrderByEnum.Author)
+            {
+                if (getOrderByDirection(orderByClause) == OrderByDirection.Desc)
+                    orderedReleases = releases.OrderByDescending(r => r.Extension.Author);
+                else
+                {
+                    orderedReleases = releases.OrderBy(r => r.Extension.Author);
+                }
+            }
+            else
+            {
+                orderedReleases = releases;
+            }
+            result.Releases = orderedReleases.Select(r => new Galleries.Domain.Model.Release(r, root)).ToArray();
 
-            result.TotalCount = releases.Count();
+            result.TotalCount = orderedReleases.Count();
             return result;
         }
 
